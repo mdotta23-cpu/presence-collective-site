71781da4e7f30b84acdb4de11b4dc6da006d9a05
mdotta23-cpu <mdotta23@gmail.com>
2026-08-18 12:17:42 +1000

    Keep refreshed mobile navigation working

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-const toggle=document.querySelector('.menu-toggle');const nav=document.querySelector('.site-header nav');toggle?.addEventListener('click',()=>nav.classList.toggle('open'));document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
+const toggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.site-header nav');
+if (toggle && nav) {
+  toggle.addEventListener('click', () => nav.classList.toggle('open'));
+}
