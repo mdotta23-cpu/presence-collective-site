f2f246654b72543d784be7cdf042b73bf1282e12
mdotta23-cpu <mdotta23@gmail.com>
2026-08-18 16:46:58 +1000

    Improve mobile navigation behaviour

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const toggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.site-header nav');
-if (toggle && nav) {
-  toggle.addEventListener('click', () => nav.classList.toggle('open'));
+const toggle=document.querySelector('.menu-toggle');
+const nav=document.querySelector('.site-header nav');
+if(toggle&&nav){
+  toggle.addEventListener('click',()=>nav.classList.toggle('open'));
+  document.querySelectorAll('nav a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
 }
